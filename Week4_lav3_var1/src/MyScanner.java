bb66c268a5d472182b398b1cc2c6045b117fc0d3
Bogdan-Teodora <teodorabogdan2002@gmail.com>
2023-11-01 17:50:03 +0200

    some changes

--- Week4_lav3_var1/src/MyScanner.java
+++ Week4_lav3_var1/src/MyScanner.java
@@ -41,6 +41,27 @@ public class MyScanner {
         return fileContent.toString().replace("\t", "");
     }
 
+
+    /**
+     * Within this method, we go through each string from tokensToBe and look in what case are we:
+     * We can have 4 cases:
+     * 1) the case when we are managing a string
+     * -- a) where we are either at the start of the string and we start to create it
+     * -- b) we found the end of the string so we add it to our final list + the line on which it is situated
+     * 2) the case when we are managing a char
+     * -- a) where we are either at the start of the char and we start to create it
+     * -- b) we found the end of the char so we add it to our final list + the line on which it is situated
+     * 3) the case when we have a new line
+     * -- we simply increase the line number in this case
+     * 4) the case when:
+     * -- a) if we have a string, we keep compute the string
+     * -- b) if we have a char, we compute the char
+     * -- c) if the token is different from " " (space) it means we found a token and we add it to our final list
+     + the line on which it is situated
+     * Basically, in this method we go through the elements of the program and for each of them, if they
+     compose a token/identifier/constant we add it to the final list and we compute also the line number on
+     which each of the are situated. (we somehow tokenize the elems which compose the program)
+     */
     private List<Pair<String, Pair<Integer, Integer>>> tokenize(List<String> tokensToBe){
 
         List<Pair<String, Pair<Integer, Integer>>> resultedTokens = new ArrayList<>();
@@ -92,6 +113,45 @@ public class MyScanner {
         return resultedTokens;
     }
 
+
+    private List<Pair<String, Pair<Integer, Integer>>> createListOfProgramsElems() {
+        try{
+            String content = this.readFile();
+            String separatorsString = this.separators.stream().reduce("", (a,b)->(a + b));
+            StringTokenizer tokenizer = new StringTokenizer(content, separatorsString, true);
+
+            /*
+              i use separatorsString to split the program into a list of string where we have
+              stored the tokens + identifiers + constants + the separators from the created string
+            */
+            List<String> tokens = Collections.list(tokenizer)
+                    .stream()
+                    .map(t->(String) t)
+                    .collect(Collectors.toList());
+
+            return tokenize(tokens);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    /**
+     * In this method, we scan the list of created tokens and we classify each of them in a category:
+     * a) 2 - for reservedWords
+     * b) 3 - for operators
+     * c) 4 - for separators
+     * d) 0 - for constants
+     * e) 1 - for identifiers
+     * If the token is a constant or an identifier we add it to the Symbol Table
+     * After figuring out the category, we add them to the ProgramInternalForm + their position in the
+     symbol table ( (-1, -1) for anything that is not a constant and an identifier ) + their category (0, 1, 2, 3, 4)
+     * If the token is not in any of the categories, we print a message with the line of the error + the token
+     which is invalid.
+     */
     public void scan() {
         List<Pair<String, Pair<Integer, Integer>>> tokens = this.createListOfProgramsElems();
         AtomicBoolean lexicalErrorExists = new AtomicBoolean(false);
@@ -104,7 +164,7 @@ public class MyScanner {
                     this.pif.add(new Pair<>(token, new Pair<>(-1, -1)), 3);
                 } else if (this.separators.contains(token)) {
                     this.pif.add(new Pair<>(token, new Pair<>(-1, -1)), 4);
-                } else if (Pattern.compile("^0|[-|+][1-9]([0-9])*|'[1-9]'|'[a-zA-Z]'|\"[0-9]*[a-zA-Z ]*\"$").matcher(token).matches()) {
+                } else if (Pattern.compile("^0|[-|+][1-9]([0-9])*|'[0-9]'|'[a-zA-Z]'|\"[0-9]*[a-zA-Z ]*\"$").matcher(token).matches()) {
                     this.symbolTable.add(token);
                     this.pif.add(new Pair<>(token, this.symbolTable.findPositionOfTerm(token)), 0);
                 } else if (Pattern.compile("^([a-zA-Z]|_)|[a-zA-Z_0-9]*").matcher(token).matches()) {
@@ -124,26 +184,6 @@ public class MyScanner {
             }
 
         }
-    }
-
-    private List<Pair<String, Pair<Integer, Integer>>> createListOfProgramsElems() {
-        try{
-            String content = this.readFile();
-            String separatorsString = this.separators.stream().reduce("", (a,b)->(a + b));
-            StringTokenizer tokenizer = new StringTokenizer(content, separatorsString, true);
-
-            List<String> tokens = Collections.list(tokenizer)
-                    .stream()
-                    .map(t->(String) t)
-                    .collect(Collectors.toList());
-
-            return tokenize(tokens);
-        }
-        catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
     public ProgramInternalForm getPif() {
